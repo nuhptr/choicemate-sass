208f26dda8838a0cc34ec3bc1d1b17b310f20f2f
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2023-11-16 22:56:27 +0700

    layout/update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-   title: "Choicemate",
+   title: "ChoiceMate | AI platform to help you make better decisions",
    description: "AI platform to help you make better decisions",
 }
 
