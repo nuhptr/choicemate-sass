acc7d00b10901152bd9a3b9b775bfa1d9890a8d1
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2023-11-16 22:56:13 +0700

    middleware/update code

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from "@clerk/nextjs"
 export default authMiddleware({
    // By default, if a user is not authenticated, they will be redirected to /sign-in.
    // add publicRoutes to override this behavior.
-   publicRoutes: ["/"],
+   publicRoutes: ["/", "/api/webhook"],
 })
 
 export const config = {
